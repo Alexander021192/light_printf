cabe3ca255f664eaf41d4ee7eaba8bacaba0e2e0
Alexander021192 <savchenko.am.vl@gmail.com>
2021-03-15 12:13:38 +0300

    some func for parser

--- for_parser/utils_for_parser.c
+++ for_parser/utils_for_parser.c
@@ -119,6 +119,20 @@ char	*ft_strtrim(char const *str, char const *set)
 	return (ft_substr(str, start, end - start));
 }
 
+int		ft_get_num(char **str)
+{
+	int	num = 0;
+	if(*str)
+		while(!ISDIGIT(**str))
+			(*str)++;
+	while(ISDIGIT(**str))
+	{
+		num = num * 10 + (**str - '0');
+		(*str)++;
+	}
+	return num;
+}
+
 int		get_resolution(t_all *all, char *str, int* check_settings, int tex_index)
 {
 	int max_width = 1024;
@@ -127,31 +141,25 @@ int		get_resolution(t_all *all, char *str, int* check_settings, int tex_index)
 	int	win_height = 0;
 
 	//mlx_get_screen_size(all->win.ptr, &max_width, &max_height);
-	printf("{%s} {%d}{%d}\n", str, win_width, win_height);
-	if(str)
-		while(*str == ' ')
-			str++;
-	while(ISDIGIT(*str))
-		win_width = win_width * 10 + (*str++ - '0');
+	// printf("{%s} {%d}{%d}\n", str, win_width, win_height);
 
+	win_width = ft_get_num(&str);
 	if(win_width > max_width)
 		return(printf("error. Win_width too large\n"));
 	else if(win_width < MIN_WIN_WIDTH)
 		return(printf("error. Win_width too low\n"));
 
-	while(*str == ' ')
-			str++;
-	while(ISDIGIT(*str))
-		win_height = win_height * 10 + (*str++ - '0');
-
+	win_height = ft_get_num(&str);
 	if(win_height > max_height)
 		return(printf("error. win_height too large\n"));
 	else if(win_height < MIN_WIN_HEIGHT)
 		return(printf("error. win_height too low\n"));
 
+	all->win_height = win_height;
+	all->win_width = win_width;
 	check_settings[tex_index] = 1;
 
-	printf("{%s} {%d}{%d}\n", str, win_width, win_height);
+	
 	return(0);
 	//return(ft_strdup(str));
 }
@@ -161,19 +169,45 @@ int		get_texpath(t_all *all, char *str, int* check_settings, int tex_index)
 	char	*tex_path;
 	tex_path = ft_strtrim(str, " ");
 	if(!tex_path)
-		return(printf("error with texture"));
+		return(printf("error with texture path"));
 	all->tex[tex_index].path = tex_path;
 	check_settings[tex_index] = 1;
-	printf("texture {%s}\n", all->tex[tex_index].path);
+	
 	return (0);
 }
 
+int		create_color(int b, int g, int r) //поменяли местями красный и синий
+{
+	if(r > 255 || g > 255 || b > 255)
+	{
+		printf("error with back color\n");
+		return(-1);
+	}
+	return(r << 16 | g << 8 | b);
+}
 
-// void 	*get_backcolor(t_all *all, char *str, int* check_settings, int tex_index)
-// {
-// 	check_settings[tex_index] = 1;
-// 	return(ft_strdup(str));
-// }
+int 	get_backcolor(t_all *all, char *str, int* check_settings, int tex_index)
+{
+	int color = 0;
+	//printf("{%s}\n", str);
+
+	color = create_color(ft_get_num(&str), ft_get_num(&str), ft_get_num(&str));
+	if(color >= 0)
+	{
+		if(tex_index == 6)
+		{
+			all->floor_color = color;
+			check_settings[tex_index] = 1;
+		}
+		if(tex_index == 7)
+		{
+			all->ceil_color = color;
+			check_settings[tex_index] = 1;
+		}
+	}
+	return(0);
+	//return(ft_strdup(str));
+}
 
 //check settings
 
@@ -182,8 +216,25 @@ int main(void)
 	t_all	all;
 	char	*str = "  640    480     ";
 	char	*str1 = "   ./textures/NO.xpm    ";
-	int		check_settings[5];
-	get_resolution(&all, str, check_settings, 1);
-	get_texpath(&all, str1, check_settings, 2);
+	char	*str2 = " 100 , 200, 150";
+	char	*str3 = " 0 , 255, 150";
+	int		check_settings[8];
+	
+	get_resolution(&all, str, check_settings, 0);
+	for(int i = 1; i < 6; i++)
+		get_texpath(&all, str1, check_settings, i);
+	get_backcolor(&all, str2, check_settings, 6);
+	get_backcolor(&all, str3, check_settings, 7);
+
+	printf("all win {%d}{%d}\n",all.win_width, all.win_height);
+	printf("texture {%s}\n", all.tex[2].path);
+	printf("back color {%d}{%d}\n", all.ceil_color, all.floor_color);
+
+	int i = -1;
+	while(i++ < 7)
+	{
+		if(!check_settings[i])
+			printf("(%d){%d} !\nexit game\n", i, check_settings[i]);
+	}
 	return 0;
 }
